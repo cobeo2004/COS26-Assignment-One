01e7b75d0bebd66a65f44321e085327227a870aa
Nathan <nathanwijaya33@gmail.com>
2023-04-28 15:19:39 +1000

    add processEOI.php page

---
+++ processEOI.php
@@ -0,0 +1,44 @@
+<?php
+// initialise variables
+$job_reference_number = "";
+$first_name = "";
+$last_name = "";
+$date_of_birth = "";
+$gender = "";
+$street_address = "";
+$suburb = "";
+$state = "";
+$postcode = "";
+$skills = [];
+$other_skills = "";
+$status = "";
+
+// Check if this page is reloaded after a form submission
+if (isset($_POST["submit"])) {
+	// connect to database
+	
+	// save the form data to variables
+
+    // Sanitise inputs
+
+	// Validation
+	$error = false;
+
+	// DATA TYPE CHECKS
+
+	// RANGE CHECKS
+	
+	// EXISTENCE CHECKS
+
+	// If there is no error, add the application to the database
+	if ($error == false) {
+		
+		}
+
+		
+	}
+
+
+// If there is an error, display the error messages and fill the inputs with the user's previous data (in the HTML form)
+?>
+
