3304fe20f1ce6a31fa56a1204fb47ce5fcd64b66
cobeo2004 <minh030963@stu.vinschool.edu.vn>
2023-05-12 15:05:29 +1000

    Update settings.php profile (don't use my outlook, I store censored videos there xD)

--- settings.php
+++ settings.php
@@ -1,8 +1,10 @@
 <?php
     $db_hostname = "feenix-mariadb.swin.edu.au";
-    $db_username = "nil";
-    $db_userpassword = "nil";
-    $db_name = "nil";
+    $db_username = "s103819212";
+    $db_userpassword = "Nethunter_28122004";
+    $db_name = "s103819212";
+    $connection = @mysqli_connect($db_hostname, $db_username, $db_userpassword, $db_name)
+
 ?>
 
 
