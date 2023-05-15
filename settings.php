73b59d3a502e2259a0e0e35e65fe086ecbeb34b9
cobeo2004 <minh030963@stu.vinschool.edu.vn>
2023-05-15 13:14:48 +1000

    Add the function to create table

--- settings.php
+++ settings.php
@@ -6,10 +6,19 @@
     $table = "cars";
     $connection = @mysqli_connect($host_name, $user_name, $password, $database);
 
-    function check_if_exists_table($conncetion, $table) {
+    function check_if_connected(\mysqli $connection) {
         if(mysqli_connect_errno()) {
             die("Connect failed! \n".mysqli_connect_error());
-            exit();
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    function check_if_exists_table(\mysqli $conncetion, string $table) {
+        if(!check_if_connected($conncetion)) {
+            return false;
         }
         else {
             $result = mysqli_query($conncetion, "DESCRIBE `$table`;");
@@ -24,7 +33,14 @@
         }
     }
 
-
+    function create_table(\mysqli $connection, string $table) {
+        if(check_if_exists_table($connection, $table)) {
+            echo("<p> Already got the data table</p>");
+        }
+        else {
+            mysqli_query($connection, "CREATE TABLE $table");
+        }
+    }
 ?>
 
 
