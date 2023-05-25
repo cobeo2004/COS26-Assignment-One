7c093c4daec05909ac56efcd440db62125d8bd99
cobeo2004 <minh030963@stu.vinschool.edu.vn>
2023-05-25 20:26:23 +1000

    idk why

--- apply.php
+++ apply.php
@@ -5,10 +5,14 @@ created: 29-Mar-2023
 description: Job application page
 -->
 
+
+
+<!-- TODO: make confirmation page -->
+
 <?php
-session_start();
 // check if there are form errors indicated in the URL parameter
 if (isset($_GET['error'])) {
+  session_start();
   // set error to true
   $error = true;
   // get the error messages from session variables
